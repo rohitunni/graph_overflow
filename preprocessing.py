1e2ce99f0407a33258914d8b24d9b1ff556b11ee
Rohit Unni <rohitunni11@gmail.com>
2018-01-05 13:35:07 -0800

    added parse_code, made into class

--- preprocessing.py
+++ preprocessing.py
@@ -4,28 +4,46 @@ import numpy as np
 import re
 
 
-def parse_tags(tag_string):
-    '''
-    Parses the tags from the raw text in the dataset
+class Preprocessor(object):
 
-    Input: single string of tags matching pattern "<tag>"
+    def __init__(self):
+        pass
 
-    Output: set of strings of the tags inside the <>
-    '''
-    tag_list = re.findall('<[-\w\.]*>', tag_string)
-    tag_list = [tag[1:-1] for tag in tag_list]
+    def parse_tags(tag_string):
+        '''
+        Parses the tags from the raw text in the dataset
 
-    return set(tag)
+        Input: single string of tags matching pattern "<tag>"
 
-def parse_code_blocks(raw_text):
-    '''
-    Takes raw HTML text and splits into two strings of all words contained
-    inside code blocks and all words outside of code blocks
+        Output: set of strings of the tags inside the <>
+        '''
+        tag_list = re.findall('<[-\w\.]*>', tag_string)
+        tag_list = [tag[1:-1] for tag in tag_list]
 
-    Input: Raw HTML string
-    Output: Non-code string, Code string
-    '''
+        return set(tag)
 
-    soup = BeautifulSoup(raw_text)
+    def parse_code_blocks(raw_text):
+        '''
+        Takes raw HTML text and splits into two strings of all words contained
+        inside code blocks and all words outside of code blocks
 
-    pass
+        Input: Raw HTML string
+        Output: Non-code string, Code string
+        '''
+
+        soup = BeautifulSoup(raw_text)
+        b = soup.body
+        code_blocks = b.find_all('code')
+
+        code_tags = [tag.extract() for tag in code_blocks]
+        code_strings = [tag.text for tag in code_tags]
+        code = ''
+        for string in code_strings:
+            code += string
+            code += ' '
+
+        no_code = b.text
+        no_code = re.sub('\n', '', no_code)
+
+
+        return no_code, code
