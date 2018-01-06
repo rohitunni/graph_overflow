13e236066e8606594fbc36994ffac04b6530afff
Rohit Unni <rohitunni11@gmail.com>
2018-01-05 18:04:53 -0800

    added more preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -9,7 +9,7 @@ class Preprocessor(object):
     def __init__(self):
         pass
 
-    def parse_tags(tag_string):
+    def parse_tags(self, tag_string):
         '''
         Parses the tags from the raw text in the dataset
 
@@ -17,12 +17,12 @@ class Preprocessor(object):
 
         Output: set of strings of the tags inside the <>
         '''
-        tag_list = re.findall('<[-\w\.]*>', tag_string)
+        tag_list = re.findall('<[-\w\.]*>', str(tag_string))
         tag_list = [tag[1:-1] for tag in tag_list]
 
-        return set(tag)
+        return set(tag_list)
 
-    def parse_code_blocks(raw_text):
+    def parse_code_blocks(self, raw_string):
         '''
         Takes raw HTML text and splits into two strings of all words contained
         inside code blocks and all words outside of code blocks
@@ -30,8 +30,7 @@ class Preprocessor(object):
         Input: Raw HTML string
         Output: Non-code string, Code string
         '''
-
-        soup = BeautifulSoup(raw_text)
+        soup = BeautifulSoup(str(raw_string), 'lxml')
         b = soup.body
         code_blocks = b.find_all('code')
 
@@ -43,7 +42,36 @@ class Preprocessor(object):
             code += ' '
 
         no_code = b.text
-        no_code = re.sub('\n', '', no_code)
+        no_code = re.sub('\n|\r', '', no_code)
+
+        return {'no code': no_code, 'code': code}
+
+    def test_function(self, num):
+        return num * 0
+
+    def transform(self, data):
+        #id, title, body, q_score, answer, a_score, tags
+        X = np.copy(data)
+        num_rows = len(X)
+        # first separate the two text bodies into code and non code texts
+        #X[:,2] = self.parse_code_blocks(X[:,2])
+        #X[:,4] = self.parse_code_blocks(X[:,4])
+        #break the raw tag strings into sets of tags
+        empty_cols = np.zeros((num_rows, 4))
+        X = np.concatenate((X, empty_cols), axis = 1)
+        print X.shape
+
+        for row in X:
+            row[6] = self.parse_tags(row[6])
+            row[7] = self.parse_code_blocks(row[2])['no code']
+            row[8] = self.parse_code_blocks(row[2])['code']
+            row[9] = self.parse_code_blocks(row[4])['no code']
+            row[10] = self.parse_code_blocks(row[4])['code']
 
 
-        return no_code, code
+
+
+        #X[:,6] = self.parse_tags(X[:,6])
+
+
+        return X
