779ac520a6f0a05527a5d60bab47b9ab44cf3321
Rohit Unni <rohitunni11@gmail.com>
2018-01-10 11:37:19 -0800

    started main graph class

--- featurizer.py
+++ featurizer.py
@@ -3,21 +3,46 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
 from sklearn.decomposition import NMF
+from code_tokenizer import code_tokenizer
 
 
 class Featurizer(object):
 
-    def __init__(self, n_features = 10):
+    def __init__(self, n_features = 10, start_column = 5):
+        self.s = start_column
         self.n_features = n_features
+        self.nmf_ncode = None
+        self.nmf_code = None
+
+    def make_feature_matrix(self, X, is_code = False):
+        if is_code:
+            tfidf = TfidfVectorizer(tokenizer = code_tokenizer)
+        else:
+            tfidf = TfidfVectorizer()
+        full_matrix = tfidf.fit_transform(X)
+
+        nmf = NMF(n_components = self.n_features)
+
+        
+
+        reduced_matrix = nmf.fit_transform(full_matrix)
+
+        return reduced_matrix
 
 
     def fit_transform(self, X):
-        
-        non_code_texts = np.concatenate((X[:,3], X[:, 5]), axis = 0)
-        code_texts = np.concatenate((X[:,4], X[:,6]))
 
-        tfidf_ncode = TfidfVectorizer()
-        non_code_matrix = tfidf_ncode.fit_transform(non_code_texts)
+        non_code_texts = np.concatenate((X[:,self.s], X[:, self.s+2]), axis = 0)
+        code_texts = np.concatenate((X[:,self.s+1], X[:,self.s+3]), axis = 0)
 
-        tfidf_code = TfidfVectorizer(tokenizer=code_tokenizer)
-        code_matrix = tfidf_code.fit_transform(code_texts)
+        non_code_matrix = self.make_feature_matrix(non_code_texts, is_code = False)
+
+        code_matrix = self.make_feature_matrix(code_texts, is_code = True)
+
+        rejoined_ncode = np.concatenate(np.split(non_code_matrix, 2, axis = 0), axis = 1)
+
+        rejoined_code = np.concatenate(np.split(code_matrix, 2, axis = 0), axis = 1)
+
+        full_matrix = np.concatenate([rejoined_ncode, rejoined_code], axis = 1)
+
+        return full_matrix
---
+++ graph_overflow.py
@@ -0,0 +1,18 @@
+import networkx as nx
+import nxpd as nxd
+import numpy as np
+import pandas as pd
+from preprocessing import Preprocessor
+from featurizer import Featurizer
+
+class Graph_Overflow(Object):
+
+    def __init__(self):
+
+        pass
+
+    def 
+
+    def make_digraph(self, data):
+
+        pass
