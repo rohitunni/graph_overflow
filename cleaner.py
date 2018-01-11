5205e3f0f803fef5b2fc039e68c3ce37eb263194
Rohit Unni <rohitunni11@gmail.com>
2018-01-11 15:25:55 -0800

    cleaning file

---
+++ cleaner.py
@@ -0,0 +1,15 @@
+from preprocessing import Preprocessor
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('stackodata.csv')
+
+p = Preprocessor()
+
+cleaned_data = p.transform(df.values)
+
+df_cleaned = pd.DataFrame(cleaned_data, columns = ['id', 'title', 'qscore', 'ascore',
+                                                   'tags', 'q_nocode', 'q_code',
+                                                   'a_nocode', 'a_code'])
+
+df_cleaned.to_csv('stack_data_cleaned.csv', encoding = 'utf-8')
