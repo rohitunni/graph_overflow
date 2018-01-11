a8d90a8be44a0c30599ad02b7da3e4a72a32b6e0
Rohit Unni <rohitunni11@gmail.com>
2018-01-11 14:29:00 -0800

    wednesday changes

--- graph_overflow.py
+++ graph_overflow.py
@@ -5,27 +5,31 @@ import pandas as pd
 from preprocessing import Preprocessor
 from featurizer import Featurizer
 from sklearn.metrics.pairwise import cosine_distances
+from scipy.spatial.distance import cosine as cos_dist
 
 class Graph_Overflow(object):
 
-    def __init__(self, ncode_features = 10, code_features = 10):
+    def __init__(self, ncode_features = 10, code_features = 10, threshold = 1):
         self.nc = ncode_features
         self.c = code_features
         self.qa = self.nc + self.c
         self.G = None
         self.distances = []
+        self.threshold = threshold
         pass
 
     def get_similarities(self, data):
         edge_list = []
         for i, node in enumerate(data):
             for j, other_node in enumerate(data):
+                best_node_dist = (None, 1)
+                qualifying_edges = []
                 if i != j:
-                    dist = cosine_distances(node[self.qa:].reshape(1,-1), other_node[:self.qa].reshape(1,-1))
-                    edge_list.append((i, j, dist))
-                    self.distances.append(dist)
+                    dist = cos_dist(node[self.qa:], other_node[:self.qa])
+                    if dist < self.threshold:
+                        qualifying_edges.append((i, j, dist))
+                        self.distances.append(dist)
 
-        self.distances = np.array(self.distances)
         return edge_list
 
 
@@ -34,7 +38,7 @@ class Graph_Overflow(object):
         self.G = nx.DiGraph()
         self.G.add_weighted_edges_from(edge_list)
 
-        pass
+        return self.G
 
     def prune_edges(self, edge_list):
 
