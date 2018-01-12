e1828c8a5f67e34359976bd4352d6883584daab9
Rohit Unni <rohitunni11@gmail.com>
2018-01-11 16:56:04 -0800

    ran script for featurizer

---
+++ make_matrix_1.py
@@ -0,0 +1,11 @@
+from featurizer import Featurizer
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('stack_data_cleaned.csv', na_filter=False)
+
+f = Featurizer(n_features = 20)
+
+matrix_data = f.fit_transform(df.values)
+
+np.savetxt('first_nmf_20.csv', matrix_data, delimiter=',')
